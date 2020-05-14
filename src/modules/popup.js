e97b859156b1e77502907d79ae4f96991bdb7fc3
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-05-14 13:32:36 +0300

    1st and new branch for POPUP

---
+++ src/modules/popup.js
@@ -0,0 +1,32 @@
+'use strict';
+try{
+    const contacts = document.querySelectorAll('.contacts'),
+        popupCall = document.querySelector('.popup-call'),
+        btnPopupClose = document.querySelector('.popup-close');
+
+    const showEl = (el) => {
+        el.style.display = 'block';
+    };
+    const hideEl = (el) => {
+        el.style.display = 'none';
+    };
+
+    contacts.forEach(block => {
+        block.addEventListener('click', (ev) => {
+            showEl(popupCall);
+        });
+    });
+    document.body.addEventListener('click', (ev) => {
+        if (ev.target === btnPopupClose || //нажатие на крестик
+            ev.target.closest('.popup-content') && ev.target.tagName !== 'INPUT' && ev.target.tagName !== 'BUTTON' ||
+            //нажатие на поле popup, кроме инпутов и кнопки
+            !ev.target.closest('.popup-content') && !ev.target.closest('.contacts')){
+            //нажатие на любой элемент страницы, кроме модального окна и кнопки вызова popup
+            hideEl(popupCall);
+        }
+    });
+} catch(e){ 
+    console.warn(e);
+}
+
+
