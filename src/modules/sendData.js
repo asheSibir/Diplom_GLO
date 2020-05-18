4d58b72ecaff86f083277aab6164bdb58952817a
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-05-18 19:10:50 +0300

    POST is switched off

--- src/modules/sendData.js
+++ src/modules/sendData.js
@@ -4,8 +4,7 @@ const sendData = () => {
         const forms = document.querySelectorAll('form'),
             errorMessage = 'Что-то пошло не так...',
             loadMessage = 'Загрузка...',
-            successsMessage = 'Спасибо! Мы скоро с Вами свяжемся!';
-            
+            successsMessage = 'Спасибо! Мы скоро с Вами свяжемся!';  
         
         //ВАЛИДАЦИЯ ВВОДОВ
         const validate = (data) => {
@@ -49,6 +48,7 @@ const sendData = () => {
         forms.forEach(form => {
             validate(form);
         });
+        
         //ЛОАДЕР
         const preloader = () => {
             return `
@@ -63,71 +63,15 @@ const sendData = () => {
                 <div></div>
             </div>`;
         }; 
-        // forms.forEach((form) => {
-        //     const submit = form.querySelector('[name="submit"]');
-        //     submit.remove();
-        // });
         
-
         //ОТПРАВКА ФОРМЫ 
+        document.addEventListener('submit', (ev) => {
+            console.log(ev);
+        });
         forms.forEach((form) => {
-            const spinner = preloader();
             form.addEventListener('submit', (ev) => {
-                event.preventDefault();
-                const smallText = form.querySelector('.small'),
-                smallMessage = smallText.textContent;
-                smallText.textContent = loadMessage;
-                smallText.style.cssText = 'font-size: 2rem; color: #19b5fe';
-                if (!document.getElementById('loader')){
-                    form.insertAdjacentHTML('beforeend', spinner); 
-                }
-                const formData = new FormData(form);
-                let body = {};
-                for (let val of formData.entries()) {
-                body[val[0]] = val[1];
-                }
-                const postData = (body, form) => { 
-                    return fetch('server.php', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(body), 
-                        credentials: 'include'
-                    }); 
-                };
-                postData(body, form)
-                .then((response) => {
-                    if(response.status !== 200){
-                        throw new Error(response.statusText);
-                    }
-                    return response.body;                    
-                })
-                .then((data) => {
-                    smallText.textContent = successsMessage;
-                    setTimeout(() => {
-                        form.closest('.popup').style.display = 'none';
-                        smallText.textContent = smallMessage;
-                        smallText.style = '';
-                        document.getElementById('loader').remove();
-                    }, 3000);
-                })
-                .catch((err) => {
-                    console.warn(err);
-                    smallText.textContent = errorMessage;
-                    
-                    setTimeout(() => {
-                        smallText.textContent = 'Попробуйте еще раз или позвоните нам! Мы ответим на все Ваши вопросы';
-                        smallText.style.cssText = `
-                        font-size: 1.5rem;
-                        font-weight: bold;
-                        padding: 0 3.3em;
-                        color: #f3960d;
-                        `;
-                        //document.getElementById('loader').style.display = "none";
-                    }, 3000);
-                });
-            });
+                console.log(111);
+            });    
         });
 
     } catch(e){
