0209c01189c3027e476e5e186c537e067f851281
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-05-18 20:24:30 +0300

    A part of remarks has been treated. (Send forma and calc are missing

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -16,31 +16,23 @@ const movePopUp = () => {
         //Открытие разных POPUP
         document.body.addEventListener('click', (ev) => {
             const target = ev.target;
-            if (target.closest('.contacts')){
+            if (target.closest('.call-btn')){ //При клике на эту надписи в header и footer 
                 showEl(document.querySelector('.popup-call'));
             }
-            if (target.closest('.discount-btn')){
+            if (target.closest('.discount-btn')){ //после нажатия на кнопку “Получить расчет и скидку” 
                 showEl(document.querySelector('.popup-discount'));
             }
-            if (target.closest('.check-btn')){
+            if (target.closest('.check-btn')){ //При нажатии на кнопку “Получить чек-лист и скидку”
                 showEl(document.querySelector('.popup-check'));
             }
-            if (target.closest('.consultation-btn')){
+            if (target.closest('.consultation-btn')){ //открывается модальное окно с классом popup-consultation
                 showEl(document.querySelector('.popup-consultation'));
-            }
-            if (target.closest('.main-form-btn')){
-                showEl(document.querySelector('.popup-consultation'));
-            }
-            if (target.closest('.capture-form-btn')){
-                showEl(document.querySelector('.popup-discount'));
             }
         });
         //Закрытие POPUP
         document.body.addEventListener('click', (ev) => {
             ev.preventDefault();
             if (ev.target.closest('.popup-close') || //нажатие на крестик
-                ev.target.closest('.popup-content') && ev.target.tagName !== 'INPUT' && ev.target.tagName !== 'BUTTON' ||
-                //нажатие на поле popup, кроме инпутов и кнопки
                 !ev.target.closest('.popup-content') && !ev.target.closest('.contacts')){
                 //нажатие на любой элемент страницы, кроме модального окна и кнопки вызова popup
                 hideEl(ev.target.closest('.popup'));
