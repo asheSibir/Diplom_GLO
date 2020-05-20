f11ac000bba627c73eb6b31c9ff86c8da4796b43
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-05-20 11:04:07 +0300

    Almost ready for IE (calc is strange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 //POLYFILLS for IE
 import '@babel/polyfill'; //https://babeljs.io/docs/en/babel-polyfill
+import 'nodelist-foreach-polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
+
+import 'formdata-polyfill';
+import "regenerator-runtime/runtime.js";
+import 'element-remove';
+import 'url-polyfill';
+import 'es6-promise';
+import 'fetch-polyfill';
+import 'whatwg-fetch';
+import 'scroll-behavior-polyfill';
+
 
 
 //Importing scripts from modules
@@ -8,9 +21,9 @@ import sendData from './modules/sendData';
 import moveAccordion from './modules/accordion';
 import calcul from './modules/calcul';
 import showMore from './modules/showMore';
-import postData from './modules/postData';
 import postForma from './modules/postForma';
 import putInStorage from './modules/putInStorage';
+
 
 //ЗАПУСК ФУНКЦИЙ
 //POPUP (все 4)
